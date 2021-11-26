e19eace32f9250ba51683a3612ab59c8769a9edb
thelamer <ryankuba@gmail.com>
2021-11-26 12:35:05 -0800

    update dev setup for new assets

--- setup_dev.sh
+++ setup_dev.sh
@@ -1,23 +1,5 @@
 #! /bin/bash
 
-# Working emulators for retroarch
-retroarchemus="\
-fceumm
-snes9x
-mednafen_vb
-gearboy
-vba_next
-gambatte
-genesis_plus_gx
-handy
-mame2003_plus
-mednafen_ngp
-mednafen_wswan
-o2em
-prboom
-tyrquake
-vecx
-bluemsx"
 IFS=$'\n'
 rootdir=$(pwd)
 # Install modules
@@ -30,20 +12,8 @@ curl -o \
 tar xf \
   /tmp/emulatorjs-blob.tar.gz -C \
   frontend/ --strip-components=1
-# Libretro emscripten
-mkdir retrotmp
-cd retrotmp
-wget https://buildbot.libretro.com/stable/1.9.10/emscripten/RetroArch.7z
-7z x RetroArch.7z
-sed -i 's/wasmBinaryFile="/wasmBinaryFile="data\//g' retroarch/*.js
-for emu in $retroarchemus; do
-  mv retroarch/${emu}_libretro.* "${rootdir}/frontend/data/"
-done
-cd retroarch/assets/frontend/bundle/
-zip -r frontend.zip  assets/xmb/monochrome assets/ozone shaders filters info autoconfig overlay assets/menu_widgets
-mv frontend.zip "${rootdir}/frontend/data/"
-cd "${rootdir}"
-rm -Rf retrotmp
+  rm -f \
+    frontend/data/{snes*,sega*,vb*,ws*,a2600*,a7800*,arcade*,bluemsx*,gb*,jaguar*,lynx*,mame*,msx*,nds*,nes*,ngp*,pce*,saturn*,psx*}
 # Custom cores
 curl -o \
   /tmp/custom-cores.tar.gz -L \
