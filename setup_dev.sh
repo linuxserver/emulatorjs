e2800587bedb9dd4c1d513edeb12b11ba1721274
thelamer <ryankuba@gmail.com>
2022-02-23 12:16:35 -0800

    repoint dev script to forked repo

--- setup_dev.sh
+++ setup_dev.sh
@@ -8,13 +8,10 @@ npm install
 # EmulatorJS
 curl -o \
   /tmp/emulatorjs-blob.tar.gz -L \
-  "https://github.com/ethanaobrien/emulatorjs/archive/main.tar.gz"
+  "https://github.com/thelamer/emulatorjs/archive/main.tar.gz"
 tar xf \
   /tmp/emulatorjs-blob.tar.gz -C \
   frontend/ --strip-components=1
-  rm -f \
-    frontend/data/{snes*,sega*,vb*,ws*,a2600*,a7800*,arcade*,bluemsx*,gb*,jaguar*,lynx*,mame*,msx*,nds*,nes*,ngp*,pce*,saturn*,psx*}
-  rm -Rf frontend/docs
 # Custom cores
 curl -o \
   /tmp/custom-cores.tar.gz -L \
